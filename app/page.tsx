7d92403ad5ad9808aaa3198e30f3877773dcce76
Andriy Nosov <nosovandriy@gmail.com>
2023-03-18 21:00:04 +0200

    feature/hero add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import CatalogPreview from "@catalog-preview/catalog-preview";
 import ProductCard from "./catalog/[cakeId]/page";
+import HeroSection from "@hero/hero";
 
 export default function Home() {
   return (
     <>
+    <HeroSection />
       <CatalogPreview />
     </>
   );
