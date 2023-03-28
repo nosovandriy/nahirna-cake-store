afe628ea90aa5e8e356c7dbf62fe9946c824ac2a
Andriy Nosov <nosovandriy@gmail.com>
2023-03-28 12:45:04 +0300

    feature/may-like add component

--- app/cart/empty-cart/empty-cart.tsx
+++ app/cart/empty-cart/empty-cart.tsx
@@ -1,16 +1,20 @@
 "use client";
 
+import MayLikeProducts from "@may-like/may-like";
 import Link from "next/link";
 
 const EmptyCart = () => {
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-10 my-auto pb-10">
-      <h2>Твоя корзина порожня</h2>
+    <>
+      <div className="my-[120px] flex h-full flex-col items-center justify-center gap-10 pb-10">
+        <h2>Твоя корзина порожня</h2>
 
-      <Link href={"/catalog"}>
-        <button className="primaryButton">Перейти в каталог </button>
-      </Link>
-    </div>
+        <Link href={"/catalog"}>
+          <button className="primaryButton">Перейти в каталог </button>
+        </Link>
+      </div>
+      <MayLikeProducts />
+    </>
   );
 };
 
