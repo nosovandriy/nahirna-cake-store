e6336b65fdc423c442b852f7beea561eac38621b
Andriy Nosov <nosovandriy@gmail.com>
2023-05-03 23:07:35 +0300

    change price, update next to 13.3.4

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -7,12 +7,12 @@ const NotFoundPage = () => {
   const router = useRouter();
 
   return (
-    <div className="flex flex-col items-center justify-center my-auto">
+    <div className="my-auto flex flex-col items-center justify-center">
       <div className="flex items-center justify-center gap-5 tablet:gap-8">
-        <h2 className="text-[140px] tablet:text-[264px] font-[800px] leading-[100%] desktop:text-[264px]">
+        <h2 className="text-[140px] font-[800px] leading-[100%] tablet:text-[264px] desktop:text-[264px]">
           4
         </h2>
-        <div className="w-[100px] h-[100px] tablet:h-[175px] tablet:w-[175px]">
+        <div className="h-[100px] w-[100px] tablet:h-[175px] tablet:w-[175px]">
           <Image
             src="/assets/404.webp"
             alt="cheesecake"
@@ -23,14 +23,18 @@ const NotFoundPage = () => {
           />
         </div>
 
-        <h2 className="text-[140px] tablet:text-[264px] font-[800px] leading-[100%] desktop:text-[264px]">
+        <h2 className="text-[140px] font-[800px] leading-[100%] tablet:text-[264px] desktop:text-[264px]">
           4
         </h2>
       </div>
-      <h3 className="mb-8 tablet:mb-10 text-base tablet:text-lg">Вибачте, такої сторінки не знайдено...</h3>
-      <button className="primaryButton px-8" onClick={() => router.push("/")}>
-        Повернутись на головну
-      </button>
+      <h3 className="mb-8 text-base tablet:mb-10 tablet:text-lg">
+        Вибачте, такої сторінки не знайдено...
+      </h3>
+      <a href={"/"} className="py-3">
+        <button className="primaryButton px-8">
+          Повернутись на головну
+        </button>
+      </a>
     </div>
   );
 };
